3040a039342b486d0468e604b79d95d93e29caa7
João Tedeschi <joaorafaelbt@gmail.com>
2023-09-21 15:24:29 -0300

    [Changed] JSON Method

--- src/etl/transformers.py
+++ src/etl/transformers.py
@@ -67,7 +67,7 @@ class SFTransformer(Transformer):
         """
         self.dataframe = dataframe
         logging.info(f'Transforming {len(self.dataframe)} records')
-        schema = json.loads(Path(schema)) if schema else None
+        schema = json.load(Path(schema)) if schema else None
 
         if schema:
             # drop columns that are not in the schema
