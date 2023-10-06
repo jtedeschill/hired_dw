77b13476b19756ad4710fc5d01e38e55a151048e
João Tedeschi <joaorafaelbt@gmail.com>
2023-10-06 16:17:18 -0300

    [Added] New SF Object

---
+++ src/etl/queries/sfdc_lead.sql
@@ -0,0 +1,17 @@
+SELECT
+id,
+CreatedDate,
+Disqualified_Reasons__c, 
+Entered_Disqualified_Date__c, 
+howDidYouHearAboutUs__c, 
+Nurture_Reasons__c,
+Entered_Nurture_Date__c,
+Status,
+Disqualified_Other__c,
+Nurture_Other__c,
+type__c
+
+
+
+FROM Lead
+WHERE (type__c != 'Job Seeker' OR type__c = NULL)
