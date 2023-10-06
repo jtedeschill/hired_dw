4f1ab00ea9638262ee35b789af56cfdebf8478dc
bhrenopaladinoLL <145780468+bhrenopaladinoLL@users.noreply.github.com>
2023-10-06 19:56:58 +0000

    Update sfdc_campaign_history.sql
    Added Lead Id to the query

--- src/etl/queries/sfdc_campaign_history.sql
+++ src/etl/queries/sfdc_campaign_history.sql
@@ -1,6 +1,7 @@
 SELECT
 id,
 contactid,
+LeadId, 
 CreatedDate,
 FCRM__FCR_QR__c,
 FCRM__FCR_QR_Date__c,
@@ -8,4 +9,5 @@ campaign.name,
 campaign.type
 
 FROM campaignmember
-WHERE Contact.RecordType.Name != 'Candidate'
+WHERE (Contact.RecordType.Name != 'Candidate' OR Contact.RecordType.Name = null)
+AND (Lead.type__c != 'Job Seeker' OR Lead.type__c = null)
