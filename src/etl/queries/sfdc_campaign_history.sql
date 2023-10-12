31fd94078576b621a631b10557c36824437b12e7
bhrenopaladinoLL <145780468+bhrenopaladinoLL@users.noreply.github.com>
2023-10-12 14:59:57 +0000

    Update sfdc_campaign_history.sql
    adding last modified to the pipeline. this will be used as criteria for inbound owner

--- src/etl/queries/sfdc_campaign_history.sql
+++ src/etl/queries/sfdc_campaign_history.sql
@@ -11,7 +11,9 @@ campaign.Intent_Type__c,
 Status, 
 MQL_Owner_Role__c,
 MQL_Owner__c,
-FCRM__FCR_First_Owner_Worked__c
+FCRM__FCR_First_Owner_Worked__c,
+FCRM__FCR_Last_Modified_By_Formula__c,
+FCRM__FCR_Last_Modified_By_Date_Formula__c
 
 FROM campaignmember
 WHERE (Contact.RecordType.Name != 'Candidate' OR Contact.RecordType.Name = null)
