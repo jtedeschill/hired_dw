066c299c61f9b4b4355af8a1044aa8f8a3c51853
João Tedeschi <joaorafaelbt@gmail.com>
2023-11-22 15:21:54 -0300

    [Changed] Applied timezone localizing to LA

--- src/etl/transformers.py
+++ src/etl/transformers.py
@@ -82,6 +82,9 @@ class SFTransformer(Transformer):
                 # if it is a date, convert to datetime
                 if data_type == 'date':
                     self.dataframe[column_name] = pd.to_datetime(self.dataframe[column_name].copy(), errors='coerce')
+                    # apply timezone conversion
+                    self.dataframe[column_name] = self.dataframe[column_name].copy().dt.tz_convert('America/Los_Angeles')
+                    
                 # otherwise, convert to the specified data type
                 else:
                     self.dataframe[column_name] = self.dataframe[column_name].copy().astype(data_type)
