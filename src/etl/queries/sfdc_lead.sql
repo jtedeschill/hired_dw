5985709605f32a2d053527bc0ea8e681e348e3e0
bhrenopaladinoLL <145780468+bhrenopaladinoLL@users.noreply.github.com>
2023-11-27 18:07:45 -0300

    Update sfdc_lead.sql
    addint utms to the Lead

--- src/etl/queries/sfdc_lead.sql
+++ src/etl/queries/sfdc_lead.sql
@@ -16,7 +16,17 @@ Job_Function__c,
 Job_Level__c,
 UserGems__Is_a_UserGem__c,
 LD_BookIt_Log_ID__c,
-Account_Segment__c
+Account_Segment__c,
+UTM_Campaign__c,
+Original_UTM_Campaign__c, 
+UTM_Source__c,
+Original_UTM_Source__c, 
+UTM_Medium__c, 
+Original_UTM_Medium__c, 
+UTM_Content__c, 
+Original_UTM_Content__c, 
+UTM_Description__c, 
+Original_UTM_Description__c
 
 FROM Lead
 WHERE (type__c != 'Job Seeker' OR type__c = NULL)
