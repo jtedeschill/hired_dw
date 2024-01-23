ed44a74c82784ed1c77922ac658716039bcfd879
GuiAugustoG <153561776+GuiAugustoG@users.noreply.github.com>
2024-01-23 10:09:28 -0300

    Update sfdc_campaign_history.sql

--- src/etl/queries/sfdc_campaign_history.sql
+++ src/etl/queries/sfdc_campaign_history.sql
@@ -15,7 +15,9 @@ FCRM__FCR_First_Owner_Worked__c,
 FCRM__FCR_Last_Modified_By_Formula__c,
 FCRM__FCR_Last_Modified_By_Date_Formula__c,
 FCRM__FCR_Inquiry_Target__c,
-Account_ID__c
+Account_ID__c,
+LastModifiedById,
+LastModifiedBy.Name
 FROM campaignmember
 WHERE (Contact.RecordType.Name != 'Candidate' OR Contact.RecordType.Name = null)
 AND (Lead.type__c != 'Job Seeker' OR Lead.type__c = null)
